046973d713b5e955be5e5be5fc4301aa6f0642e0
Mr-indescribable <st.indescribable@gmail.com>
2019-01-31 16:35:01 +0800

    修改 id 生成器的测试脚本

--- Neverland/test/id_gen.py
+++ Neverland/test/id_gen.py
@@ -11,26 +11,25 @@ from neverland.components.idgeneration import IDGenerator
 id_generator = IDGenerator(0x01, 0x01)
 
 
-generated_ids = set()
-
-
 # Test case for IDGenerator
 class IDGTest(unittest.TestCase):
 
     def test_0_gen(self):
+        current = None
+        previous = None
+
         for _ in (range(1000000)):
             id_ = id_generator.gen()
-            b2_len = len('{0:b}'.format(id_))
 
+            previous = current
+            current = id_
+            self.assertNotEqual(current, previous)
+
+            base2_str = '{0:b}'.format(id_)
+            b2_len = len(base2_str)
             self.assertEqual(b2_len, 64)
-            self.assertNotIn(id_, generated_ids)
 
-            generated_ids.add(id_)
-
-            print(id_)
-            print('{0:b}'.format(id_))
-            print('----------------')
-
+            print(base2_str)
 
 
 if __name__ == '__main__':
