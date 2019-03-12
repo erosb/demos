b12b3fe6fe2040c71fcd3265e835b49addbdde6f
Mr-indescribable <st.indescribable@gmail.com>
2019-03-12 17:54:48 +0800

    微小的调整

--- Neverland/neverland/components/connmgmt.py
+++ Neverland/neverland/components/connmgmt.py
@@ -3,7 +3,7 @@
 
 
 from neverland.utils import ObjectifiedDict, MetaEnum
-from neverland.communicate.shm import SharedMemoryManager
+from neverland.components.shm import SharedMemoryManager
 from neverland.node.context import NodeContext
 
 
@@ -12,10 +12,11 @@ from neverland.node.context import NodeContext
 Nodes in the Neverland cluster shall complete a fake connection before
 they start to communicate with each other.
 
-Due to we only transport UDP packets in the cluster, so we call this connection
-as "a fake connection". It's not like the TCP connection. The only thing we
-need to do is send an IV to the recipient and comfirm the recipient has recived
-the IV.
+Due to we only transport UDP packets in the cluster, so we call this
+connection as "a fake connection". It's not like the TCP connection.
+The only thing we need to do is send an IV to the recipient and comfirm
+the recipient has recived the IV. And then, this IV will be used in
+the encryption of the established connection.
 '''
 
 
