669565951fc527944ba90a6eb8dd0addba1611c6
Mr-indescribable <st.indescribable@gmail.com>
2019-04-02 18:09:53 +0800

    美化 kc 的 unittest

--- Neverland/test/kc.py
+++ Neverland/test/kc.py
@@ -2,6 +2,7 @@
 #coding: utf-8
 
 import os
+import time
 import struct
 import unittest
 
@@ -16,30 +17,55 @@ config_json = {
         'identification': 'a l00o00oOOoOOoo00oOOo00ong identification string',
         'crypto': {
             'password': 'a SUPER SUPER LONG AND VERY INDESCRIBABLE pASSw0rD',
-            'cipher': 'kc-aes-192-gcm'
+            'cipher': 'kc-aes-256-gcm'
         }
     }
 }
 config = OD(**config_json)
 
 
-data_4_test = os.urandom(40000)
-
-
 gcm_cipher = GCMKernelCryptor(config, Modes.ENCRYPTING)
 gcm_decipher = GCMKernelCryptor(config, Modes.DECRYPTING)
+
+tsum_urandom = 0
+tsum_crypto = 0
 
 
 # Test case for kernel cryptors
 class IDGTest(unittest.TestCase):
 
     def test_0_gcm(self):
-        cipher_text = gcm_cipher.update(data_4_test)
-        plain_text = gcm_decipher.update(cipher_text)
+        global tsum_urandom, tsum_crypto
 
-        print('GCM testing data length: ', len(data_4_test))
-        print('GCM cipher text length: ', len(cipher_text))
-        self.assertEqual(plain_text, data_4_test)
+        times = 50000
+        bs = 65535
+        total_mb = times * bs / 1024 / 1024
+        total_mb = round(total_mb, 2)
+        cipher_name = config.net.crypto.cipher
+
+        print(
+            f'Running {times} times of {cipher_name} cipher testing with data '
+            f'block size {bs}. \nTotal: {total_mb} MB\n'
+        )
+
+        for _ in range(times):
+            t0 = time.time()
+            data_4_test = os.urandom(bs)
+            t1 = time.time()
+
+            tsum_urandom += t1 - t0
+
+            t0 = time.time()
+            cipher_text = gcm_cipher.update(data_4_test)
+            plain_text = gcm_decipher.update(cipher_text)
+            t1 = time.time()
+
+            tsum_crypto += t1 - t0
+
+            self.assertEqual(plain_text, data_4_test)
+
+        print(f'Seconds spent on generating random data: {tsum_urandom}')
+        print(f'Seconds spent on encrypting & decrypting: {tsum_crypto}')
 
 
 if __name__ == '__main__':
